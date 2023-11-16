e90033eaf00a0b598ba9cebced6da51925790432
jws <350331045@qq.com>
2023-11-16 09:03:13 +0800

    Update screen_utils.dart

--- lib/util/screen_utils.dart
+++ lib/util/screen_utils.dart
@@ -42,37 +42,37 @@ class Screen {
   static double diagonalInches(BuildContext context) => diagonal(context) / _ppi;
 
   static double get widthOt {
-    MediaQueryData mediaQuery = MediaQueryData.fromWindow(ui.window);
+    MediaQueryData mediaQuery = MediaQueryData.fromView(ui.window);
     return mediaQuery.size.width;
   }
 
   static double get heightOt {
-    MediaQueryData mediaQuery = MediaQueryData.fromWindow(ui.window);
+    MediaQueryData mediaQuery = MediaQueryData.fromView(ui.window);
     return mediaQuery.size.height;
   }
 
   static double get scale {
-    MediaQueryData mediaQuery = MediaQueryData.fromWindow(ui.window);
+    MediaQueryData mediaQuery = MediaQueryData.fromView(ui.window);
     return mediaQuery.devicePixelRatio;
   }
 
   static double get textScaleFactor {
-    MediaQueryData mediaQuery = MediaQueryData.fromWindow(ui.window);
+    MediaQueryData mediaQuery = MediaQueryData.fromView(ui.window);
     return mediaQuery.textScaleFactor;
   }
 
   static double get navigationBarHeight {
-    MediaQueryData mediaQuery = MediaQueryData.fromWindow(ui.window);
+    MediaQueryData mediaQuery = MediaQueryData.fromView(ui.window);
     return mediaQuery.padding.top + kToolbarHeight;
   }
 
   static double get topSafeHeight {
-    MediaQueryData mediaQuery = MediaQueryData.fromWindow(ui.window);
+    MediaQueryData mediaQuery = MediaQueryData.fromView(ui.window);
     return mediaQuery.padding.top;
   }
 
   static double get bottomSafeHeight {
-    MediaQueryData mediaQuery = MediaQueryData.fromWindow(ui.window);
+    MediaQueryData mediaQuery = MediaQueryData.fromView(ui.window);
     return mediaQuery.padding.bottom;
   }
 
