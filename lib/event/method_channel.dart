0b597cfb975c85d8d55ec132ccc876b704fe771a
lpp <350331045@qq.com>
2024-08-24 08:42:24 +0800

    Update method_channel.dart

--- lib/event/method_channel.dart
+++ lib/event/method_channel.dart
@@ -2,7 +2,7 @@ import 'package:flutter/services.dart';
 
 class FlutterPlugin {
   static const MethodChannel _channel =
-      const MethodChannel('flutter_method_channel');
+      MethodChannel('flutter_method_channel');
 
   static toast(String msg) {
     _channel.invokeMethod('toast', msg);
